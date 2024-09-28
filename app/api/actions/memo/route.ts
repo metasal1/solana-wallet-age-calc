5ea13c2d8ab3d7456b21ae1d8088e373fc200fbe
Sal Samani <sal@milysec.com>
2024-09-28 21:28:47 +1000

    memo demo

--- app/api/actions/memo/route.ts
+++ app/api/actions/memo/route.ts
@@ -54,7 +54,7 @@ export const POST = async (req: Request) => {
         }
 
         const connection = new Connection(
-            process.env.SOLANA_RPC! || clusterApiUrl('devnet'),
+            process.env.SOLANA_RPCM! || clusterApiUrl('mainnet-beta'),
         );
 
         const transaction = new Transaction().add(
