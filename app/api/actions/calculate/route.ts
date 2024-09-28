068f820170f399787bff7d401c10738f7daddfd5
Sal Samani <sal@milysec.com>
2024-09-28 22:08:42 +1000

    wallet age blink;

--- app/api/actions/calculate/route.ts
+++ app/api/actions/calculate/route.ts
@@ -1,10 +1,10 @@
 import {
     ActionPostResponse,
     createPostResponse,
-    MEMO_PROGRAM_ID,
     ActionGetResponse,
     ActionPostRequest,
     createActionHeaders,
+    MEMO_PROGRAM_ID,
 } from '@solana/actions';
 import {
     clusterApiUrl,
@@ -14,7 +14,6 @@ import {
     Transaction,
     TransactionInstruction,
 } from '@solana/web3.js';
-
 
 const headers = createActionHeaders({
     chainId: "mainnet", // or chainId: "devnet"
@@ -33,8 +32,6 @@ export const GET = async (req: Request) => {
     });
 };
 
-// DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
-// THIS WILL ENSURE CORS WORKS FOR BLINKS
 export const OPTIONS = async () => {
     return new Response(null, { headers });
 };
@@ -42,7 +39,7 @@ export const OPTIONS = async () => {
 export const POST = async (req: Request) => {
     try {
         const body: ActionPostRequest = await req.json();
-
+        console.log(body);
         let account: PublicKey;
         try {
             account = new PublicKey(body.account);
@@ -52,17 +49,38 @@ export const POST = async (req: Request) => {
                 headers,
             });
         }
+        const walletPayload = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/api/wallet-age', {
+            method: 'POST',
+            body: JSON.stringify({ "walletAddress": account.toBase58() }),
+        });
 
+        const age = await walletPayload.json();
+        console.log(age);
         const connection = new Connection(
             process.env.SOLANA_RPCM! || clusterApiUrl('mainnet-beta'),
         );
 
-        const transaction = new Transaction()
+        const transaction = new Transaction().add(
+            // note: `createPostResponse` requires at least 1 non-memo instruction
+            ComputeBudgetProgram.setComputeUnitPrice({
+                microLamports: 1000,
+            }),
+            new TransactionInstruction({
+                programId: new PublicKey(MEMO_PROGRAM_ID),
+                data: Buffer.from('this is a simple memo message2', 'utf8'),
+                keys: [],
+            }),
+        );
 
+        transaction.feePayer = account;
+
+        transaction.recentBlockhash = (
+            await connection.getLatestBlockhash()
+        ).blockhash;
         const payload: ActionPostResponse = await createPostResponse({
             fields: {
                 transaction,
-                message: 'Post this memo on-chain',
+                message: `Your Wallet's (${account.toBase58()}) age is ${age.ageInDays} days`,
                 type: 'transaction',
             },
             // no additional signers are required for this transaction
