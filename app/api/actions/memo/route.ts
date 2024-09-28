7e5b77c4c520ead1bb5d7eaab5ae2734955efcff
Sal Samani <sal@milysec.com>
2024-09-28 21:20:13 +1000

    memo demo

--- app/api/actions/memo/route.ts
+++ app/api/actions/memo/route.ts
@@ -1,15 +1,11 @@
-/**
- * Solana Actions Example
- */
-
 import {
     ActionPostResponse,
-    ACTIONS_CORS_HEADERS,
     createPostResponse,
     MEMO_PROGRAM_ID,
     ActionGetResponse,
     ActionPostRequest,
-} from "@solana/actions";
+    createActionHeaders,
+} from '@solana/actions';
 import {
     clusterApiUrl,
     ComputeBudgetProgram,
@@ -17,24 +13,29 @@ import {
     PublicKey,
     Transaction,
     TransactionInstruction,
-} from "@solana/web3.js";
+} from '@solana/web3.js';
+
+// create the standard headers for this route (including CORS)
+const headers = createActionHeaders();
 
 export const GET = async (req: Request) => {
     const payload: ActionGetResponse = {
-        title: "Actions Example - Simple On-chain Memo",
-        icon: new URL("/solana_devs.jpg", new URL(req.url).origin).toString(),
-        description: "Send a message on-chain using a Memo",
-        label: "Send Memo",
+        title: 'Actions Example - Simple On-chain Memo',
+        icon: new URL('/solana_devs.jpg', new URL(req.url).origin).toString(),
+        description: 'Send a message on-chain using a Memo',
+        label: 'Send Memo',
     };
 
     return Response.json(payload, {
-        headers: ACTIONS_CORS_HEADERS,
+        headers,
     });
 };
 
 // DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
 // THIS WILL ENSURE CORS WORKS FOR BLINKS
-export const OPTIONS = GET;
+export const OPTIONS = async () => {
+    return new Response(null, { headers });
+};
 
 export const POST = async (req: Request) => {
     try {
@@ -46,12 +47,12 @@ export const POST = async (req: Request) => {
         } catch (err) {
             return new Response('Invalid "account" provided', {
                 status: 400,
-                headers: ACTIONS_CORS_HEADERS,
+                headers,
             });
         }
 
         const connection = new Connection(
-            process.env.SOLANA_RPC! || clusterApiUrl("devnet"),
+            process.env.SOLANA_RPC! || clusterApiUrl('devnet'),
         );
 
         const transaction = new Transaction().add(
@@ -61,7 +62,7 @@ export const POST = async (req: Request) => {
             }),
             new TransactionInstruction({
                 programId: new PublicKey(MEMO_PROGRAM_ID),
-                data: Buffer.from("this is a simple memo message2", "utf8"),
+                data: Buffer.from('this is a simple memo message2', 'utf8'),
                 keys: [],
             }),
         );
@@ -76,22 +77,23 @@ export const POST = async (req: Request) => {
         const payload: ActionPostResponse = await createPostResponse({
             fields: {
                 transaction,
-                message: "Post this memo on-chain",
+                message: 'Post this memo on-chain',
+                type: 'transaction',
             },
             // no additional signers are required for this transaction
             // signers: [],
         });
 
         return Response.json(payload, {
-            headers: ACTIONS_CORS_HEADERS,
+            headers,
         });
     } catch (err) {
         console.log(err);
-        let message = "An unknown error occurred";
-        if (typeof err == "string") message = err;
+        let message = 'An unknown error occurred';
+        if (typeof err == 'string') message = err;
         return new Response(message, {
             status: 400,
-            headers: ACTIONS_CORS_HEADERS,
+            headers,
         });
     }
 };
